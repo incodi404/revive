4474a43cb4118d491eab3130ffa1c6c4b7682822
Dipankar Chowdhury <dipankarnn2123@gmail.com>
2025-12-04 22:17:43 +0530

    update

---
+++ src/stacks/RootStack.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Signin from '../screens/Signin';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const RootStack = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <>
+      <Stack.Navigator initialRouteName='Signin'>
+        <Stack.Screen name="Signin" component={Signin} />
+      </Stack.Navigator>
+    </>
+  );
+};
+
+export default RootStack;
